a66e03753e85d29dc797f12ad5f4101adb61d1c7
RamonGiovane <ramon.giovane@inovamind.com.br>
2021-11-24 01:05:36 -0300

    Separando action types em constantes

--- src/store/actions/numbers.js
+++ src/store/actions/numbers.js
@@ -1,7 +1,8 @@
+import { CHANGE_MIN_NUM, CHANGE_MAX_NUM } from "../reducers/actionTypes"
 //Action Creator
 export function changeMinNumber(newValue){
   return {
-    type: 'CHANGE_MIN_NUM',
+    type: CHANGE_MIN_NUM,
     payload: newValue
   }
 }
@@ -9,7 +10,7 @@ export function changeMinNumber(newValue){
 //Action Creator
 export function changeMaxNumber(newValue){
   return {
-    type: 'CHANGE_MAX_NUM',
+    type: CHANGE_MAX_NUM,
     payload: newValue
   }
 }
--- src/store/reducers/numbers.js
+++ src/store/reducers/numbers.js
@@ -1,13 +1,15 @@
+import {CHANGE_MAX_NUM, CHANGE_MIN_NUM} from './actionTypes'
+
 const initialState =  { min: 1, max: 10 }
 export default function (state = initialState, action) {
 
   switch (action.type) {
-    case 'CHANGE_MIN_NUM':
+    case CHANGE_MIN_NUM:
       return {
         ...state,
         min: action.payload
       }; 
-    case 'CHANGE_MAX_NUM':
+    case CHANGE_MAX_NUM:
       return {
         ...state,
         max: action.payload

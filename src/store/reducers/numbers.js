4d8e2400d6028d7bc1ebea2b8f69424b7afbb7c2
RamonGiovane <ramon.giovane@inovamind.com.br>
2021-11-24 00:37:24 -0300

    Separando Reducers do Store

---
+++ src/store/reducers/numbers.js
@@ -0,0 +1,17 @@
+const initialState =  { min: 1, max: 10 }
+export default function (state = initialState, action) {
+
+  switch (action.type) {
+    case 'CHANGE_MIN_NUM':
+      return {
+        ...state,
+        min: action.payload
+      }; 
+    case 'CHANGE_MAX_NUM':
+      return {
+        ...state,
+        max: action.payload
+      }
+    default: return state;
+  }
+}
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,21 +1,8 @@
 import { createStore, combineReducers } from "redux";
+import numbersReducer from "./reducers/numbers";
 
-const reducers = combineReducers({
-  numbers: function (state, action) {
-    switch (action.type) {
-      case 'CHANGE_MIN_NUM':
-        return {
-          ...state,
-          min: action.payload
-        }; 
-      case 'CHANGE_MAX_NUM':
-        return {
-          ...state,
-          max: action.payload
-        }
-      default: return { min: 1, max: 10 }
-    }
-  },
+const reducers = combineReducers({  
+  numbers: numbersReducer,
 
   names_example: function (state, action) {
     console.log(state, '', action)
